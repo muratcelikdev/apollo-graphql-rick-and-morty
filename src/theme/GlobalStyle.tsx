575815a239bdfd4585db98d32c746bd25d87d10a
Murat Çelik <muratcelikdev@gmail.com>
2022-06-18 00:48:22 +0300

    feat: add reset button styles

--- src/theme/GlobalStyle.tsx
+++ src/theme/GlobalStyle.tsx
@@ -9,6 +9,27 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
+
+  button {
+    border: none;
+    margin: 0;
+    padding: 0;
+    width: auto;
+    overflow: visible;
+
+    background: transparent;
+
+    color: inherit;
+    font: inherit;
+
+    line-height: normal;
+
+    -webkit-font-smoothing: inherit;
+    -moz-osx-font-smoothing: inherit;
+
+    -webkit-appearance: none;
+    cursor: pointer;
+  }
 `;
 
 export default GlobalStyle;
